ca9a40e723a078ac366a87c25ace643994f71520
inayat100 <68042481+inayat100@users.noreply.github.com>
2023-11-12 21:45:00 +0530

    Add files via upload

---
+++ barcode.py
@@ -0,0 +1,15 @@
+import keyboard
+
+def on_barcode_scanned(e):
+    barcode_data = e.name
+    process_barcode(barcode_data)
+
+def process_barcode(barcode_data):
+    # Implement your logic to handle the scanned barcode data
+    print("Scanned barcode:", barcode_data)
+
+# Register the callback function for key events
+keyboard.hook(on_barcode_scanned)
+
+# Keep the script running
+keyboard.wait('esc') 
